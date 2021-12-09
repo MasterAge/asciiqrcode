890f0f4618e3ecdf4f2a942e487d928da7714f3b
Adrian Seddon <MasterAge@bitbucket.org>
2021-12-09 23:39:39 +1100

    master: Added docs

--- README.md
+++ README.md
@@ -12,5 +12,20 @@ pip install -r requirements.txt qrtogo
 
 ## Usage
 ```bash
-python qrtogo.py <file>
+python qrtogo.py  -h
+usage: QRToGo [-h] -w WIDTH [-b BLACK] [-v] [--dump-qr-code] file
+
+Processes ASCII QR Codes.
+
+positional arguments:
+  file                  A file containing an ASCII QR code.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -w WIDTH, --width WIDTH
+                        The width of the QR code, including the whitespace.
+  -b BLACK, --black BLACK
+                        The character that represents the black squares
+  -v, --verbose         Enable verbose logging.
+  --dump-qr-code        Write the created QR code image to disc.
 ```
--- qrtogo.py
+++ qrtogo.py
@@ -7,11 +7,17 @@ from pyzbar.pyzbar import decode
 
 
 def to_image(qrcode: list, black_char: str) -> Image:
+    """
+    Converts an ASCII QR code list to a RGB image.
+    :param qrcode: The ASCII QR code.
+    :param black_char: The character in the ASCII QR code that represents the black squares.
+    :return: A QR code with a white background.
+    """
     img = Image.new('RGB', (len(qrcode[0]), len(qrcode)), 'white')
     pixels = img.load()
 
     for y, line in enumerate(qrcode):
-        for x, pixel in enumerate(line):
+        for x, _ in enumerate(line):
             if qrcode[y][x] == black_char:
                 pixels[y, x] = (0, 0, 0)
 
@@ -19,11 +25,20 @@ def to_image(qrcode: list, black_char: str) -> Image:
 
 
 def read_file(file, width) -> list:
+    """
+    Reads an ASCII QR code file into a list of lines.
+    :param file: The filename to read from.
+    :param width: The width of the QR code in pixels.
+    :return: A list of lines.
+    """
     with open(file) as f:
         qrcode = f.readlines()
 
+    # If the qr code is not already broken up.
     if len(qrcode[0]) != width:
+        # Strip all newlines.
         contents = "".join([line.strip() for line in qrcode])
+
         if len(contents) % width != 0:
             raise RuntimeError(f"QR code has characters left over after dividing by width ({width}).")
 
@@ -32,15 +47,38 @@ def read_file(file, width) -> list:
             start = i * width
             qrcode.append(contents[start:start + width])
 
-    logging.info(f"Read the following QR Code from {file}:")
-
-    for i in qrcode:
-        logging.info("  " + i)
+    # Extra spaces are needed after second newline to ensure top row of QR code is indented.
+    logging.info(f"Read the following QR Code from %s:\n\n  %s\n",
+                 file, "\n  ".join(qrcode))
 
     return qrcode
 
 
+def parse_ascii_qrcode(file: str, width: int, black: str, dump_qr_code: bool) -> str:
+    """
+    Retrieves the data from an ASCII QR code.
+    :param file: The filename to read from.
+    :param width: The width of the QR code in pixels.
+    :param black: The character in the ASCII QR code that represents the black squares.
+    :param dump_qr_code: Whether to write the QR code image to disk.
+    :return: The data stored in the QR code.
+    """
+    qrcode_chars = read_file(file, width)
+    img = to_image(qrcode_chars, black)
+
+    if dump_qr_code:
+        filename = file.split(".")[0] + ".png"
+        img.save(filename, 'PNG')
+        logging.info(f"Wrote QR code to {filename}")
+
+    return str(decode(img)[0].data, "utf-8")
+
+
 def _parse_args() -> argparse.Namespace:
+    """
+    Creates an arg parser for QRToGo and parses the supplied args.
+    :return: The parsed args.
+    """
     parser = argparse.ArgumentParser(prog="QRToGo", description='Processes ASCII QR Codes.')
     parser.add_argument('-w', '--width', type=int, required=True,
                         help='The width of the QR code, including the whitespace.')
@@ -54,18 +92,6 @@ def _parse_args() -> argparse.Namespace:
                         help='A file containing an ASCII QR code.')
 
     return parser.parse_args()
-
-
-def parse_ascii_qrcode(file: str, width: int, black: str, dump_qr_code: bool) -> str:
-    qrcode_chars = read_file(file, width)
-    img = to_image(qrcode_chars, black)
-
-    if dump_qr_code:
-        filename = file.split(".")[0] + ".png"
-        img.save(filename, 'PNG')
-        logging.info(f"Wrote QR code to {filename}")
-
-    return str(decode(img)[0].data, "utf-8")
 
 
 if __name__ == '__main__':
