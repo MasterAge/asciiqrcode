2d31eba04d3a0b3d64bbb81a1fb03367553fd069
Adrian Seddon <MasterAge@bitbucket.org>
2021-12-16 22:18:49 +1100

    master: Added tests

---
+++ test/qrtogo_test.py
@@ -0,0 +1,30 @@
+import os
+import unittest
+
+from parameterized import parameterized
+
+import qrtogo
+
+
+class MyTestCase(unittest.TestCase):
+    digitalcube_soln = "HTB{QR_!snt_d34d}"
+
+    @parameterized.expand([
+        ("digitalcube_1.txt", digitalcube_soln),
+        ("digitalcube_split.txt", digitalcube_soln),
+        ("digitalcube_inverse.txt", digitalcube_soln)
+    ])
+    def test_qrcode_files(self, file, output):
+        data = qrtogo.parse_ascii_qrcode("test_files/" + file)
+        self.assertEqual(output, data)
+
+    def test_dump_qr_code(self):
+        file = "test_files/digitalcube_1.txt"
+        qrtogo.parse_ascii_qrcode(file, True)
+
+        image = file.replace("txt", "png")
+        self.assertTrue(os.path.isfile(image))
+
+
+if __name__ == '__main__':
+    unittest.main()
